65d0c34aaef8891faefdad467e50d19c3b3a14ac
masataka <ar.tec.masa@gmail.com>
2018-06-06 20:54:02 +0900

    タイトルを作成

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
 // Componentはreactに入っているライブラリ
 
-import Greeting from './greeting';
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'John',
     };
-  }
-
-  handleNameChange(name) {
-    this.setState({ name });
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}
-        />
-        <button onClick={() => this.handleNameChange('Bob')}>I am Bob</button>
-        <Greeting name={this.state.name} />
+        <h1>緯度経度検索</h1>
       </div>
     );
   }
