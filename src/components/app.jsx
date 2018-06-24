4f8a68b8c7fabf708d823b2d609fc1d98fa29aa6
masataka <ar.tec.masa@gmail.com>
2018-06-24 23:57:16 +0900

    google MAP使用できなくなったけど、コミット

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,6 +6,7 @@ import Map from './Map';
 import HotelsTable from './HotelsTable';
 
 import { geocode } from '../domain/Geocoder';
+import { searchHotelByLocation } from '../domain/HotelRepository';
 
 class App extends Component {
   constructor(props) {
@@ -38,7 +39,7 @@ class App extends Component {
         switch (status) {
           case 'OK': {
             this.setState({ address, location });
-            break;
+            return searchHotelByLocation(location);
           }
           case 'ZERO_RESULTS': {
             this.setErrorMessage('結果が見つかりませんでした');
@@ -48,6 +49,10 @@ class App extends Component {
             this.setErrorMessage('エラーが発生しました');
           }
         }
+        return [];
+      })
+      .then((hotels) => {
+        this.setState({ hotels });
       })
       .catch(() => {
         this.setErrorMessage('通信に失敗しました');
---
+++ src/domain/HotelRepository.js
@@ -0,0 +1,18 @@
+import Rakuten from '../lib/Rakuten';
+
+const RAKUTEN_APP_ID = '1048685660900958190';
+
+
+// eslint-disable-next-line import/prefer-default-export
+export const searchHotelByLocation = (location) => {
+  const params = {
+    applicationId: RAKUTEN_APP_ID,
+    datumType: 1,
+    latitude: location.lat,
+    longitude: location.lng,
+  };
+  return Rakuten.Travel.simpleHotelSearch(params)
+    .then((result) => {
+      console.log(result);
+    });
+};
